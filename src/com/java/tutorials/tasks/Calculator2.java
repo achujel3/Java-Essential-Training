7fa809752e87c1a5797a15f3d9d02010a3d937b2
achujelas123 <achujel3@gmail.com>
2017-03-01 04:47:22 +0200

    calc2

---
+++ src/com/java/tutorials/tasks/Calculator2.java
@@ -0,0 +1,50 @@
+package com.java.tutorials.tasks;
+
+import java.util.Scanner;
+
+public class Calculator2 {
+
+    public static void main(String[] args){
+
+        String firstNumber = getInput("Write first number: ");
+        String secondNumber = getInput("Write second number: ");
+        String action = getInput("What action you want to do ( + - * /): ");
+
+        while(!firstNumber.equals("0") || !secondNumber.equals("0") || !action.equals("0")){
+            try {
+                switch (action) {
+                    case "+":
+                        System.out.println("The result is: " + (Double.valueOf(firstNumber) + Double.valueOf(secondNumber)));
+                        break;
+                    case "-":
+                        System.out.println("The result is: " + (Double.valueOf(firstNumber) - Double.valueOf(secondNumber)));
+                        break;
+                    case "*":
+                        System.out.println("The result is: " + (Double.valueOf(firstNumber) * Double.valueOf(secondNumber)));
+                        break;
+                    case "/":
+                        System.out.println("The result is: " + (Double.valueOf(firstNumber) / Double.valueOf(secondNumber)));
+                        break;
+                    default:
+                        System.out.println("You've entered a wrong action!");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("You've entered not a number!");
+            }
+
+            firstNumber = getInput("Write first number: ");
+            secondNumber = getInput("Write second number: ");
+            action = getInput("What action you want to do ( + - * /): ");
+
+        }
+
+    }
+
+    public static String getInput(String prompt){
+        System.out.println(prompt);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+
+}
