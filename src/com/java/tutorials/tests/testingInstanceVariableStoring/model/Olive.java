d4a15fd15a67433ee2a02751a9448a79b9846b8b
achujelas123 <achujel3@gmail.com>
2017-03-03 19:18:49 +0200

    testing constants

--- src/com/java/tutorials/tests/testingInstanceVariableStoring/model/Olive.java
+++ src/com/java/tutorials/tests/testingInstanceVariableStoring/model/Olive.java
@@ -2,7 +2,10 @@ package com.java.tutorials.tests.testingInstanceVariableStoring.model;
 
 public class Olive {
 
-    private String name = "Kalamata";
+    public static final String KALAMATA = "Kalamata";
+    public static final String LIGURIAN = "Ligurian";
+
+    private String name = KALAMATA;
     private long color = 0x2E0854;
     private int oil = 3;
 
--- src/com/java/tutorials/tests/testingInstanceVariableStoring/testingInstanceVariables.java
+++ src/com/java/tutorials/tests/testingInstanceVariableStoring/testingInstanceVariables.java
@@ -10,13 +10,12 @@ public class testingInstanceVariables {
     public static void main(String[] args) {
 
         List<Olive> olives = new ArrayList<>();
-        olives.add(new Olive("Kalamara", 0x2E0854, 3));
-        olives.add(new Olive("Kalamara", 0x2E0854, 3));
-        olives.add(new Olive("Ligurian", 0x000000, 2));
-        olives.add(new Olive("Ligurian", 0x000000, 2));
-        olives.add(new Olive("Ligurian", 0x000000, 2));
-        olives.add(new Olive("Ligurian", 0x000000, 2));
-
+        olives.add(new Olive(Olive.KALAMATA, 0x2E0854, 3));
+        olives.add(new Olive(Olive.KALAMATA, 0x2E0854, 3));
+        olives.add(new Olive(Olive.LIGURIAN, 0x000000, 2));
+        olives.add(new Olive(Olive.LIGURIAN, 0x000000, 2));
+        olives.add(new Olive(Olive.LIGURIAN, 0x000000, 2));
+        olives.add(new Olive(Olive.LIGURIAN, 0x000000, 2));
         OlivePress olivePress = new OlivePress();
         int totalOil = olivePress.getOil(olives);
         System.out.println("Total olive oil: " + totalOil);
