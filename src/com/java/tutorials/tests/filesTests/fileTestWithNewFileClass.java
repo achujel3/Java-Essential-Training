a73c8953833ee6f0e5284bbbedc8b577e2cf64ad
achujelas123 <achujel3@gmail.com>
2017-03-06 23:07:49 +0200

    testing file reading/wrigin with new io

---
+++ src/com/java/tutorials/tests/filesTests/fileTestWithNewFileClass.java
@@ -0,0 +1,24 @@
+package com.java.tutorials.tests.filesTests;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+public class fileTestWithNewFileClass {
+
+    public static void main(String[] args) {
+
+        String path = "src/com/java/tutorials/tests/filesTests/files";
+
+        Path sourceFile = Paths.get(path, "loremIpsum.txt");
+        Path targetFile = Paths.get(path, "target2.txt");
+
+        try {
+            Files.copy(sourceFile, targetFile, StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("File copied!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
