ec38d07c8576e1674f89ce2cadc07247aeb27d4a
achujelas123 <achujel3@gmail.com>
2017-03-06 23:49:07 +0200

    testing file input/output with apache commons

---
+++ src/com/java/tutorials/tests/filesTests/filesTestWithAppacheCommons.java
@@ -0,0 +1,24 @@
+package com.java.tutorials.tests.filesTests;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+public class filesTestWithAppacheCommons {
+
+    public static void main(String[] args) {
+
+        File sourceFile = new File("src/com/java/tutorials/tests/filesTests/files/loremIpsum.txt");
+        File targetFile = new File("src/com/java/tutorials/tests/filesTests/files/target3.txt");
+
+        try {
+            FileUtils.copyFile(sourceFile, targetFile);
+            System.out.println("File copied!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
